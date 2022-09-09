26f2675c53705bb23b6f9460c099b2f8ae0d7cc9
danstacy1 <danstacy1@gmail.com>
2022-09-09 12:07:46 -0400

    updated messages and created createuserinfo page

--- src/components/shared/AutoDismissAlert/messages.js
+++ src/components/shared/AutoDismissAlert/messages.js
@@ -6,6 +6,13 @@ const messages = {
 	signOutSuccess: 'Come back soon!',
 	changePasswordSuccess: 'Password changed successfully!',
 	changePasswordFailure:'Failed to change passwords. Check your old password and try again.',
+	getUserInfoFailure: 'Error fetching shoes.',
+	createUserInfoSuccess: 'Shoe created Successfully!',
+	createUserInfoFailure: 'Something went wrong, please try again.',
+	updateUserInfoSuccess: 'Shoe updated Successfully!',
+	updateUserInfoFailure: 'Couldnt update shoe, please try again.',
+	removeUserInfoSuccess: 'Shoe has been set free!',
+	removeUserInfoFailure: 'Couldnt remove shoe, please try again.'
 }
 
 module.exports = messages
---
+++ src/components/userInfo/CreateUserInfo.js
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import { createUserInfo } from '../../api/userInfo'
+import { useNavigate } from 'react-router-dom'
+import { createUserInfoSuccess, createUserInfoFailure } from '../shared/AutoDismissAlert/messages'
+import UserInfoForm from '../shared/UserInfoForm'
+
+const CreateUserInfo = (props) => {
+    // console.log('these are the props in createUserInfo\n', props)
+    const { user, msgAlert } = props
+
+    const navigate = useNavigate()
+
+    const [userInfo, setUserInfo] = useState({
+        firstName: '',
+        lastName: '',
+        phoneNumber: '',
+    })
+
+    console.log('this is the userInfo in createUserInfo', userInfo)
+
+
+    const handleChange = (e) => {
+        setUserInfo(prevUserInfo => {
+            let updatedValue = e.target.value
+            const updatedName = e.target.name
+            console.log('this is the input type', e.target.type)
+
+            const updatedUserInfo = {
+                [updatedName]: updatedValue
+            }
+            return {
+                ...prevUserInfo,
+                ...updatedUserInfo
+            }
+        })
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('this is the user', user)
+        console.log('this is the userInfo', userInfo)
+        createUserInfo(user, userInfo)
+            .then(res => { navigate(`/userInfo/${res.data.userInfo.id}`)})
+            .then(() => {
+                msgAlert({
+                    heading: 'Oh Yeah!',
+                    message: createUserInfoSuccess,
+                    variant: 'success'
+                })
+            })
+            .catch(() => 
+                msgAlert({
+                    heading: 'Oh No!',
+                    message: createUserInfoFailure,
+                    variant: 'danger'
+                })
+            )
+    }
+
+
+    return (
+        <UserInfoForm 
+        userInfo={ userInfo } 
+            handleChange={ handleChange }
+            handleSubmit={ handleSubmit }
+            heading="Add a new userInfo!"
+        />
+    )
+}
+
+export default CreateUserInfo
